b1f4ccdf4c709f76b590f78a6a00b8f0771702a0
Jackie Sze <jackiesze0@gmail.com>
2020-02-25 19:18:22 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -29,7 +29,7 @@ function doWhileLoop(num) {
   do {
     console.log(i, "I run once regardless.");
   }
-  while (incrementVariable() <= 9);
+  while (incrementVariable() < 9);
 }
 
 doWhileLoop(10);
