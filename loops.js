06004b957761206c5b777079700e7198184b7d8b
Jackie Sze <jackiesze0@gmail.com>
2020-02-25 19:09:39 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -22,11 +22,4 @@ function doWhileLoop(array){
 function maybeTrue() {
   return Math.random() >= 0.5;
 }
-do{
-  array.pop();
-}
-while (array.length > 0 && maybeTrue())
-return array ;
-}
-
-doWhileLoop([1,2,3,4,5])
+d
