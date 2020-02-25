84302af40044de587f42fe3603903622ea41f877
Jackie Sze <jackiesze0@gmail.com>
2020-02-25 18:27:51 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,4 +1,9 @@
 function forloop(array) {
-for (let i = 0; i < string.length; i++);
-console.log(`I am ${array[i]} strange loops.`)
+for (let i = 0; i < 25; i++);
+if (i === 1) {
+  array.push(`I am 1 strange loop.`)
+  } else {
+  array.push{`I am ${i} strange loops.`}  
+  } 
+  return array; 
 }
